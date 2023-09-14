14cc07f1cc714989b1258a0478f53801d47cb295
Diego <diegoaragao465@gmail.com>
2023-09-14 17:47:59 -0300

    Refactor model

--- user/model.py
+++ user/model.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
 
     __tablename__ = 'users'
-    id = db.Column("user_id", db.Integer, primary_key=True)
+    id = db.Column("user_id", db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
     money = db.Column(db.Double)
 
